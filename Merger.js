297268f83dabfa7189fd7155ca01f515904bdaaf
Marty Kane <marty.kane@gmail.com>
2022-03-31 16:58:51 -0500

    Test an iteration of the sort (very hard-coded for now)

--- Merger.js
+++ Merger.js
@@ -1,15 +1,51 @@
 function newSort(listOfGames) {
+    const pairsToSort = listOfGames.reduce((prev, current, i) => {
+        if (i % 2 === 0)
+            prev.push([[current]])
+        else
+            prev[prev.length - 1].push([current])
+        return prev;
+    }, []);
+
     return {
-        lists: listOfGames.map(x => [x]),
-        nextComparison: [
-            {list: 0, index: 0},
-            {list: 1, index: 0}
-        ]
+        pairsToSort,
+        nextComparison: {
+            pairIndex: 0,
+            list0Index: 0,
+            list1Index: 1
+        },
+        done: false
     }
 }
 
-function step(oldState) {
+/**
+ *
+ * @param oldState a previous sorting state object (read only)
+ * @param answer the comparison result of the oldState's "nextComparison" pair
+ * -1 = item 0 larger, 1 = item 1 larger
+ */
+function step(oldState, answer) {
+    const pairsToSort = JSON.parse(JSON.stringify(oldState.pairsToSort));
 
+    const { pairIndex, list0Index, list1Index } = oldState.nextComparison;
+    const pair = pairsToSort[pairIndex];
+    const list0 = pair[list0Index];
+    const list1 = pair[list1Index];
+
+    if (answer < 0)
+        list0.unshift(list1.pop())
+    else
+        list1.push(list0.pop())
+
+    return {
+        pairsToSort,
+        nextComparison: {
+            pairIndex: pairIndex + 1,
+            list0Index: 0,
+            list1Index: 1
+        },
+        done: false,
+    }
 }
 
 module.exports = {
--- Merger.spec.js
+++ Merger.spec.js
@@ -1,25 +1,59 @@
 const expect = require('chai').expect;
-const {newSort} = require('./Merger.js');
+const {newSort, step} = require('./Merger.js');
+
+const testInput = ['test4', 'test3', 'test1', 'test2'];
 
 describe('Starting a new sort', () => {
     it('returns a new initial data structure', () => {
-        const newState = newSort(['test4', 'test3', 'test1', 'test2']);
+        const newState = newSort(testInput);
 
-        expect(newState).to.deep.equal({
-            lists: [
-                ['test4'],
-                ['test3'],
-                ['test1'],
-                ['test2'],
+        const expectedState = {
+            pairsToSort: [
+                [
+                    ['test4'],
+                    ['test3'],
+                ],
+                [
+                    ['test1'],
+                    ['test2'],
+                ]
             ],
-            nextComparison: [
-                {list: 0, index: 0},
-                {list: 1, index: 0}
-            ]
-        });
+            nextComparison: {
+                pairIndex: 0,
+                list0Index: 0,
+                list1Index: 1
+            },
+            done: false
+        };
+
+        expect(newState).to.deep.equal(expectedState);
     });
 });
 
 describe('iterating the next step', () => {
+    it('passes in a previous state and the answer to the next comparison', () => {
+        const initialState = newSort(testInput);
+        const nextState = step(initialState, -1);
 
+        const expectedState = {
+            pairsToSort: [
+                [
+                    ['test3', 'test4'],
+                    [],
+                ],
+                [
+                    ['test1'],
+                    ['test2']
+                ]
+            ],
+            nextComparison: {
+                pairIndex: 1,
+                list0Index: 0,
+                list1Index: 1
+            },
+            done: false
+        };
+
+        expect(nextState).to.deep.equal(expectedState);
+    });
 });
